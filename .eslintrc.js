b9fe0cd1f2811274a424e54bd8da6ac131e39760
siennad <siennad97@gmail.com>
2021-05-28 19:08:33 +0300

    style (merge)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,65 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
-  },
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },  
   settings: {
     react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
     }
   },
-  extends: [
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+
   ],
-  rules: {
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  'rules': {
+    'indent': [ 'error', 2 ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
     '@typescript-eslint/no-empty-function': 'warn',
     '@typescript-eslint/no-empty-interface': 'warn',
     'prettier/prettier': [
-      'error',
+      'warn',
       {
-        endOfLine: 'auto'
+        endOfLine: 'auto',
+        bracketSpacing: true
       }
     ],
     '@typescript-eslint/no-explicit-any': 'off',
-    "space-before-blocks": ["warn", "never"]
+    'space-before-function-paren': ['warn', {
+      'anonymous': 'always',
+      'named': 'always',
+      'asyncArrow': 'always'
+    }],
+    'comma-dangle': ['warn', 'never'],
+    'no-prototype-builtins': 'warn'
   }
 };
